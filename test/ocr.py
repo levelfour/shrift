c467db5bd8e0e38373d4251ab8433e509328eb7e
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-25 10:33:03 +0900

    testスクリプトを追加

---
+++ test/ocr.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os, sys
+import itertools
+import mimetools
+import mimetypes
+from cStringIO import StringIO
+import urllib
+import urllib2
+
+class MultiPartForm(object):
+	def __init__(self):
+		self.form_fields = []
+		self.files = []
+		self.boundary = mimetools.choose_boundary()
+		return
+
+	def get_content_type(self):
+		return 'multipart/form-data; boundary=%s' % self.boundary
+
+	def add_field(self, name, value):
+		"""Add a simple field to the form data."""
+		self.form_fields.append((name, value))
+
+	def add_file(self, fieldname, filename, fileHandle="", mimetype=None):
+		"""Add a file to be uploaded."""
+		body = fileHandle.read() if fileHandle != "" else ""
+		if mimetype is None:
+			mimetype = mimetypes.guess_type(filename)[0]
+		self.files.append((fieldname, filename, mimetype, body))
+		return
+
+	def __str__(self):
+		"""Return a string representing the form data, including attached files."""
+		# リクエストの "行" を含むリストを作成する
+		# それぞれのパートはバウンダリ文字列で分割される
+		# リストが作成されると '\r\n' で区切られた行を返す
+		parts = []
+		part_boundary = '--' + self.boundary
+
+		# フォームフィールドを追加する
+		parts.extend(
+				[ part_boundary,
+					'Content-Disposition: form-data; name="%s"' % name,
+					'',
+					value,
+				] for name, value in self.form_fields)
+
+		# アップロードするファイルを追加する
+		parts.extend(
+				[ part_boundary,
+					'Content-Disposition: file; name="%s"; filename="%s"' % (field_name, filename),
+					'Content-Type: %s' % content_type,
+					'',
+					body,
+				] for field_name, filename, content_type, body in self.files)
+
+		# リストにしてバウンダリ文字列をクローズするマーカーを
+		# 追加してから CR+LF で分割されたデータを返す
+		flattened = list(itertools.chain(*parts))
+		flattened.append('--' + self.boundary + '--')
+		flattened.append('')
+		return '\r\n'.join(flattened)
+
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		raise RuntimeError('too few args')
+
+	url = sys.argv[1]
+	fpath = sys.argv[2]
+
+	form = MultiPartForm()
+	form.add_file('file', fpath)
+	request = urllib2.Request(url)
+	request.add_header('Content-type', form.get_content_type())
+	request.add_data(str(form))
+
+	print "---------- POST DATA ----------"
+	print str(form)
+
+	response = urllib2.urlopen(request)
+	print "---------- RESPONSE HEAD ----------"
+	print response.info()
+	print "---------- RESPONSE BODY ----------"
+	print response.read()
