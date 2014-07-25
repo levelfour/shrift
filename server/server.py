989d0cba7b3b56580d7f92712fa856faefefe32d
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-25 09:34:01 +0900

    ファイルアップロード機能実装(GUI)

--- server/server.py
+++ server/server.py
@@ -1,13 +1,52 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from flask import send_from_directory
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.abspath(os.path.dirname(__file__)) + '/file'
+ALLOWED_EXTENSIONS = set(['jpg', 'jpeg', 'png'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+	return '.' in filename and \
+			filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+	return 'Shrift'
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		file = request.files['file']
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			return redirect(url_for('uploaded_file',
+				filename=filename))
+	return '''
+	<!doctype html>
+	<title>Upload new File</title>
+	<h1>Upload new File</h1>
+	<form action="" method=post enctype=multipart/form-data>
+	  <p><input type=file name=file>
+		 <input type=submit value=Upload>
+	</form>
+	'''
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'],
+			filename)
+
+@app.route('/ocr')
+def ocr():
+	return 'ocr'
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
