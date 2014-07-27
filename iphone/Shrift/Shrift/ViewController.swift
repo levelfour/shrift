bb642e7ed0c23ee3deaf03b9d19d7da8bd4bbce4
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-27 23:20:16 +0900

    init XCode project

---
+++ iphone/Shrift/Shrift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shrift
+//
+//  Created by Tsutsumi Fukumu on 7/27/14.
+//  Copyright (c) 2014 ***. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
