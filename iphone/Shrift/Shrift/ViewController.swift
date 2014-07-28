8ec0a536e3d08c362431be8aaaa686eb60ec1703
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-07-28 09:14:41 +0900

    enable to draw simple lines using Core Graphics

--- iphone/Shrift/Shrift/ViewController.swift
+++ iphone/Shrift/Shrift/ViewController.swift
@@ -9,10 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-                            
+	var canvas: UIImageView!
+	var touchPoint: CGPoint!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+		
+		canvas = UIImageView(frame: UIScreen.mainScreen().bounds)
+		canvas.backgroundColor = UIColor.whiteColor()
+		self.view.addSubview(canvas)
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -20,6 +25,38 @@ class ViewController: UIViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
+	override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+		// save the beginning point of touch
+		let touch: AnyObject! = touches.anyObject()
+		touchPoint = touch.locationInView(canvas)
+	}
 
+	override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!) {
+		// save current touch point
+		let touch: AnyObject! = touches.anyObject()
+		let currentPoint: CGPoint = touch.locationInView(canvas)
+		
+		// set canvas size to drawing area
+		UIGraphicsBeginImageContext(canvas.frame.size)
+		let context: CGContext! = UIGraphicsGetCurrentContext()
+		
+		canvas.image?.drawInRect(CGRect(x: 0, y: 0, width: canvas.frame.width, height: canvas.frame.height))
+		
+		// draw line in image context
+		CGContextSetLineCap(context, kCGLineCapRound)
+		CGContextSetLineWidth(context, 10.0)
+		CGContextSetRGBStrokeColor(context, 0.0, 0.0, 0.0, 1.0)
+		CGContextMoveToPoint(context, touchPoint.x, touchPoint.y)
+		CGContextAddLineToPoint(context, currentPoint.x, currentPoint.y)
+		CGContextStrokePath(context)
+		
+		// set image to canvas
+		canvas.image = UIGraphicsGetImageFromCurrentImageContext()
+		
+		// clear graphics context
+		UIGraphicsEndImageContext()
+		
+		touchPoint = currentPoint
+	}
 }
 
