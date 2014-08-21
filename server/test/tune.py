672c255157592868ae5b1db95c1e0ccab5a83cfc
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-08-21 09:51:45 +0900

    add parameter tune script

---
+++ server/test/tune.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import numpy as np
+from sklearn import datasets, grid_search
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+
+SIZE = 10.
+
+algorithm = {
+		'svm': SVC(),
+		'rf': RandomForestClassifier()
+}
+
+parameters = {
+		'svm': {
+			'C': np.linspace(1, 20, SIZE),
+			'gamma': np.linspace(1, 2000, SIZE)
+		},
+		'rf': {
+			'n_estimators': np.linspace(5, 10000, SIZE)
+		}
+}
+
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		raise RuntimeError('too few args')
+	
+	trainX, trainY = datasets.load_svmlight_file(sys.argv[2])
+	clf = grid_search.GridSearchCV(
+			algorithm[sys.argv[1]],
+			parameters[sys.argv[1]],
+			scoring='accuracy',
+			n_jobs=-1)
+	clf.fit(trainX, trainY)
+	
+	predY = clf.predict(trainX)
+	best = clf.best_estimator_
+	print "BestEstimator: %s\nAccuracy=%s%%" \
+			% (best, accuracy_score(trainY, predY))
