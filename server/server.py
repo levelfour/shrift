1df1267defb158598a8b05dab0ea379195a24ca9
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-10-28 08:56:49 +0900

    refactor flask server

--- server/server.py
+++ server/server.py
@@ -6,10 +6,12 @@ import socket
 from config import *
 import shrift
 
+# convert illegal filename (ex. including '/') to legal filename
 def allowed_file(filename):
 	return '.' in filename and \
 			filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+# extract one character from canvas -> scaling
 def scale(file):
 	from werkzeug import FileStorage
 	from PIL import Image
@@ -17,21 +19,27 @@ def scale(file):
 	from shrift import extract_lines
 	from shrift import extract_characters
 	import numpy as np
+	# resize image
 	im = Image.open(file).convert('L').resize((400, 400))
+	# convert image to binary data
 	a = np.asarray(im.point(lambda x: 1-x/255.))
+	# extract character section
 	hsec = extract_lines(map(lambda x: np.mean(x), a))
 	vsec = extract_characters(map(lambda x: np.mean(x), a.T))
 	if len(hsec) > 1 or len(vsec) > 1:
+		# if there are multiple sections -> join to one section
 		hsec = [hsec[0][0], hsec[-1][1]]
 		vsec = [vsec[0][0], vsec[-1][1]]
 	else:
 		hsec = hsec[0]
 		vsec = vsec[0]
 
+	# extract necessary part from binary data
 	b = a[hsec[0]:hsec[1]]
 	b = b.T[vsec[0]:vsec[1]].T
 	new_im = Image.fromarray(b).resize((400, 400))
 	new_im = new_im.point(lambda x: 255*(1-x))
+	# output as jpeg image
 	buf = StringIO()
 	new_im.save(buf, 'jpeg')
 	return buf.getvalue()
@@ -41,12 +49,9 @@ def scale(file):
 ###############################
 @app.route('/')
 def index():
-	return render_template('index.html', body="""
-	<div id="canvas_wrapper">
-		<canvas id="canvas" retina="true" width=1400 height=1600>
-		</canvas> 
-	</div>
-	""")
+	with open('templates/app.html', 'r') as f:
+		html = f.read()
+	return render_template('index.html', body=html)
 
 ###############################
 # test page (train app)
@@ -83,31 +88,27 @@ def image_data(hashv):
 	response.headers['Content-Disposition'] = 'attachment; filename={}.jpg'.format(hashv)
 	return response
 
+# delete image by hash value(md5)
+@app.route('/delete/<hashv>', methods=['GET'])
+def delete_data(hashv):
+	global fs
+	objectid = fs.find({'md5': hashv})[0]._id
+	fs.delete(objectid)
+	return 'OK'
+
 @app.route('/download/', methods=['GET'])
 def make_train_data():
 	import shrift
 	shrift.generate_train_data()
-	return render_template('test.html', body="""
-	<h3>Generating train data...</h3>
-	<div id="progress">
-		<div id="loading"></div>
-	</div>
-	""")
+	with open('templates/download.html', 'r') as f:
+		html = f.read()
+	return render_template('test.html', body=html)
 
 @app.route('/add', methods=['GET'])
 def make_canvas():
-	return render_template('test.html', body="""
-	<h3>draw character in canvas</h3>
-	<canvas id="canvas" width=400 height=400 style="border: solid">
-	</canvas> 
-	<form id="canvas-form" action="/upload" method="post" encrypt="multipart/form-data">
-		<div>
-			<input id="text" type="text" name="text" placeHolder="target character" />
-		</div>
-		<button class="canvas btn-info" type="submit" id="send">Send</button>
-		<button class="canvas btn-warning" id="clear">Clear</button>
-	</form>
-	""")
+	with open('templates/add.html', 'r') as f:
+		html = f.read()
+	return render_template('test.html', body=html)
 
 # recognize characters and return response
 @app.route('/ocr', methods=['POST'])
@@ -121,24 +122,19 @@ def upload_file():
 			return shrift.ocr(secure_filename(filename))
 
 # upload train data for machine learning
-@app.route('/upload', methods=['GET'])
-def upload_form():
-	return render_template('test.html', body="""
-	<form action="/upload" method="post" enctype="multipart/form-data">
-		<input type="file" name="file" />
-		<input id="text" type="text" name="text" />
-		<button class="btn-info" type="submit">Send</button>
-	</form>
-	""")
-
-@app.route('/upload', methods=['POST'])
-def upload_train_data():
-	if request.method == 'POST':
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+	if request.method == 'GET':
+		with open('templates/upload.html', 'r') as f:
+			html = f.read()
+		return render_template('test.html', body=html)
+	elif request.method == 'POST':
 		file = request.files['file']
 		if file and allowed_file(file.filename):
 			name = secure_filename(file.filename)
 			char = request.form['text']
 			if re.split("\.", name)[0] != char:
+				# name format: <target-char>.jpg
 				name = "{}.jpg".format(char)
 			file = scale(file)
 			global fs
