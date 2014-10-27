7e22e47fc078932a660a7447fdccf5c791f6c663
Tsutsumi Fukumu <levelfourslv@gmail.com>
2014-10-27 09:22:41 +0900

    add README for server script

---
+++ server/README.md
@@ -0,0 +1,19 @@
+Shrift OCR Engine
+====
+
+# Algorithm
+
+Now using RandomForest.
+
+# Flow
+
+After input text image data, shrift processes as follows.
+(take a look at `ocr` method in shrift.py)
+
+1. extract characters from image
+... First recognizing the line, and then split line to characters.
+... At this stage, there maybe multiple candidates for extraction.
+2. recognize each extraction candidates
+... Calculate likelihood-like score, and select candidate whose score is highest. The score is calculated as follows.
+... ![](http://chart.apis.google.com/chart?cht=tx&chf=bg,s,ffffff00&chco=000000ff&chs=85&chl=%5Cfrac%7B%5Cprod_%7Bk%3D1%7D%5En%20L%7D%7Bn%7D)
+... (L: likelihood, n: length of string)
--- server/shrift.py
+++ server/shrift.py
@@ -263,35 +263,6 @@ def ocr(filename):
 
 	return ocr_str
 
-# tesseract-ocrを用いて文字認識する
-# * 英語のみ対応
-def recognize(filename, binarize=False):
-	re_ext = r'(.[A-Za-z]+)$'
-	r = re.compile(re_ext)
-	ext = r.search(filename).group(0)
-
-	if binarize:
-		# 画像をグレースケールで読み込む
-		src_image = Image.open(filename).convert("L")
-
-		# グレースケール画像を2値化
-		bin_image = src_image.point(
-				lambda col: 255 if col > 180 else 0)
-
-		# 2値化された画像を保存
-		filename = re.sub(re_ext, '_bin'+ext, filename)
-		bin_image.save(filename)
-
-	tools = pyocr.get_available_tools()
-	tool = tools[0]
-	lang = 'eng'
-
-	txt = tool.image_to_string(
-			Image.open(filename),
-			lang=lang,
-			builder=pyocr.builders.TextBuilder())
-	return txt
-
 generate_classifier()
 
 if __name__ == "__main__":
